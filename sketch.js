b36fae12bbb4572c1899a14fb36433e45a077530
Danny Callaghan <dannycallaghan@gmail.com>
2021-01-05 22:53:53 +0000

    Finishing up

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
-var stepSize = 20;
+const stepSize = 20;
+const colorOne = [255, 0, 0];
+const colorTwo = [0, 0, 255];
+const lineThickness = 2;
+const noiseDetails = 8;
 
+/**
+ * P5 setup functionality
+ * 
+ * @return void.
+ */
 function setup() {
   createCanvas(500, 500);
-
-  noiseSeed(0);
 }
-///////////////////////////////////////////////////////////////////////
+
+/**
+ * P5 draw functionality
+ * 
+ * @return void.
+ */
 function draw() {
   background(125);
 
-  noiseDetail(2);
+  noiseDetail(noiseDetails);
 
   colorGrid();
   compassGrid();
 }
-///////////////////////////////////////////////////////////////////////
+
+/**
+ * Looks after the colours in the grid
+ * 
+ * @return void.
+ */
 function colorGrid(){
+  const frame = frameCount;
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 25; j++) {
+      // Coords of tiles
       let x = i * stepSize;
       let y = j * stepSize;
-      let scale = mouseX;
-      var n = noise(x / scale, y / scale, frameCount / scale);
-      let fromColor = color(255, 0, 0);
-      let toColor = color(0, 255, 0);
+      let scale = mouseX || width; // Instantiate with a value or you can get a blank screen
+      var n = noise(x / scale, y / scale, frame / scale);
+      let fromColor = color(...colorOne);
+      let toColor = color(...colorTwo);
       let fillColor = lerpColor(fromColor, toColor, n);
       noStroke();
       fill(fillColor);
@@ -31,34 +50,48 @@ function colorGrid(){
     }
   }
 }
-///////////////////////////////////////////////////////////////////////
+
+/**
+ * Looks after the lines in the grid
+ * 
+ * @return void.
+ */
 function compassGrid(){
-
-  
-  strokeWeight(1);
-
+  strokeWeight(lineThickness);
+  const frame = frameCount;
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 25; j++) {
+      // Coords of tiles
+      const tileX = i * stepSize;
+      const tileY = j * stepSize;
+      // Coords of compasses
+      const compassX = tileX + stepSize / 2;
+      const compassY = tileY + stepSize / 2;
 
-      let x = (i * stepSize) + stepSize / 2;
-      let y = (j * stepSize) + stepSize / 2;
+      const scaleX = mouseX || width; // Instantiate with a value or you can get a blank screen
+      const scaleY = mouseY || height; // Instantiate with a value or you can get a blank screen
 
-      let scale = 5000;
-
-      var n = noise(x / scale, y / scale, frameCount / scale);
+      // Noise for rotation and colour
+      const n = noise(compassX / scaleX, compassY / scaleX, frame / scaleX);
+      // Noise for line length
+      const n2 = noise(compassX, compassY, frame / scaleY);
       
-      var r = map(n, 0, 1, 0, 720);
-
+      // Line rotation value
+      const r = map(n, 0, 1, 0, 720);
+      // Line colour one value
+      const c1 = map(n, 0, 1, 0, 255);
+      // Line colour two value
+      const c2 = map(n, 0, 1, 0, 255);
+      // Line length
+      const l = map(n2, 0, 1, 1, stepSize * 2);
 
       push()
-      stroke(0);
-      translate(x, y);
+      stroke(c1, c2, 0);
+      translate(compassX, compassY);
+      angleMode(DEGREES);
       rotate(r);
-      line(0, 0, 0, -stepSize);
+      line(0, 0, 0, -l);
       pop();
-
-      
-
     }
   }
 }
